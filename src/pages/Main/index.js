42a9a582d846eef7ef6f0d70569b06852cbded9f
Wellington N <wellingto_07@hotmail.com>
2020-08-07 10:51:13 -0300

    ultizando localStorage

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -12,6 +12,23 @@ class Main extends Component {
     loading: false,
   };
 
+  // carregar dados localStorage
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+
+    if (repositories) {
+      this.setState({ repositories: JSON.parse(repositories) });
+    }
+  }
+
+  //salvar dados do localStorage
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
+
   handleInputChange = (e) => {
     this.setState({ newRepo: e.target.value });
   };
