b9de2c46c0c2a231908e42a17cc95438bdd69b8c
Wellington N <wellingto_07@hotmail.com>
2020-08-17 16:53:56 -0300

    styled form users

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, Form, Input, SubmitButton } from './styles';
 
 export default function Main() {
-  return <Container />;
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Adicionar Usuario"
+        />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#fff" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 Main.navigationOptions = {
-  title: 'Usuario',
+  title: 'Usuários',
 };
