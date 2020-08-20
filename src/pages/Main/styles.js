37eb38e4da0d915ab693552c7506e09df450d079
Wellington N <wellingto_07@hotmail.com>
2020-08-20 10:19:21 -0300

    salvando no async storage

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -58,6 +58,27 @@ export const Name = styled.Text`
   margin-top: 4px;
   text-align: center;
 `;
-export const Bio = styled.Text``;
-export const ProfileButton = styled(RectButton)``;
-export const ProfileButtonText = styled.Text``;
+export const Bio = styled.Text.attrs({
+  numberOfLines: 2,
+})`
+  font-size: 13px;
+  line-height: 18px;
+  color: #999;
+  margin-top: 5px;
+  text-align: center;
+`;
+export const ProfileButton = styled(RectButton)`
+  margin-top: 10px;
+  align-self: stretch;
+  border-radius: 4px;
+  background: #7159c1;
+  justify-content: center;
+  align-items: center;
+  height: 36px;
+`;
+export const ProfileButtonText = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  color: #fff;
+  text-transform: uppercase;
+`;
